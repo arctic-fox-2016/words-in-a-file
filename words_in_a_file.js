73f83ddfe9389e4006fda179d78305f79c26d185
Ahyana Rizky Pratama & Liliwibi <pair+ahyanarizky.lilianti@hacktiv8.com>
2016-08-30 17:32:04 +0700

    tugas words in a file belum komplit

--- words_in_a_file.js
+++ words_in_a_file.js
@@ -1,3 +1,33 @@
 var sys=require('sys');
 
+var hasil = new Array();
+var hasilCount = new Array();
+
+function most_common_words(inputText) {
+  var words = inputText.split(' ');
+  var found, j;
+
+  for (var i=0; i < words.length; i++) {
+    found = false;
+    j = 0;
+    while (found == false && j < hasil.length) {
+      if (words[i] == hasil[j]) {
+        found = true;
+        hasilCount[j] += 1;
+      }
+      j++;
+    }
+
+    if (found == false) {
+      hasil.push(words[i]);
+      hasilCount.push(1);
+    }
+  }
+  return;
+}
 // actual  conversion code starts here
+most_common_words('apple banana apple yoghurt melon apple banana');
+
+for (i=0; i < hasil.length; i++){
+  console.log(hasil[i] + " sebanyak " + hasilCount[i]);
+}
