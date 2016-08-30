4fd8c3aab8c335c124d5147cee2c32bb442be82d
Ahyana Rizky Pratama & Liliwibi <pair+ahyanarizky.lilianti@hacktiv8.com>
2016-08-30 18:57:22 +0700

    revisi 2 word in a file

--- words_in_a_file.js
+++ words_in_a_file.js
@@ -1,11 +1,37 @@
-var sys=require('sys');
+var sys = require('util');
 
 var hasil = new Array();
 var hasilCount = new Array();
 
 function most_common_words(inputText) {
-  var words = inputText.split(' ');
-  var found, j;
+  var tanda = ["!","@","#","$","%","^","&","*","(",")","_","+","-","=",
+  "<",">",".",";", ",",":"];
+
+  var kataHubung = ["a ", "an ","the ", "and ", "by ", "or "];
+
+  var words = new Array();
+  var found;
+
+
+  for (var i = 0; i < tanda.length ; i++){
+    for (var j = 0; j < inputText.length ; j++) {
+      if (inputText.slice(j,j+1) == tanda[i]) {
+        inputText = inputText.slice(0,j) + inputText.slice(j+1,inputText.length);
+      }
+    }
+  }
+
+
+    // for (var i = 0; i < kataHubung.length ; i++){
+    //   for (var j = 0; j < inputText.length ; j++) {
+    //     if (inputText.slice(j,j+kataHubung[i].length) == kataHubung[i]) {
+    //       inputText = inputText.slice(0,j) + " " +inputText.slice(j+kataHubung[i].length,inputText.length);
+    //     }
+    //   }
+    // }
+    //
+  words = inputText.split(' ');
+
 
   for (var i=0; i < words.length; i++) {
     found = false;
@@ -26,7 +52,7 @@ function most_common_words(inputText) {
   return;
 }
 // actual  conversion code starts here
-most_common_words('apple banana apple yoghurt melon apple banana');
+most_common_words("apple! banana apple; yoghurt. melon apple banana");
 
 for (i=0; i < hasil.length; i++){
   console.log(hasil[i] + " sebanyak " + hasilCount[i]);
