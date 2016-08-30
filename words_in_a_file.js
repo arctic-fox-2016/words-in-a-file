f2b188d91aa2b03f2ec2961bc60fa14b3690011b
Kun Anggiar Lanang & Peter Raswono <pair+kun7.praswono@hacktiv8.com>
2016-08-30 14:00:46 +0700

    Second draft

--- words_in_a_file.js
+++ words_in_a_file.js
@@ -1,29 +1,42 @@
 var sys=require("sys");
 var fs=require("fs");
 
-// actual  conversion code starts here
-
 function most_common_words(source, n) {
 
     // Initialize variables
     var text = "";
+    var textArray = [];
     var dataAll = [];
     var check = 0
 
-    text = source;
+    // Load file from source
+    fs.readFile(source, function (err, data) {
+        if (err) {
+            return console.error(err);
+        }
+    });
 
-    // Convert text file to string
-    var text = source.split(" ");
+    // Synchronous read
+    var data = fs.readFileSync(source);
+
+    // Convert data to text
+    text = data.toString().toLowerCase();
+
+    // Replace all symbols and characters to space
+    text = text.replace(/\'/g, " ").replace(/\=/g, " ").replace(/\[/g, " ").replace(/\]/g, " ").replace(/\./g, " ").replace(/{]/g, " ").replace(/}]/g, " ").replace(/\*/g, " ").replace(/\|/g, " ").replace(/\//g, " ").replace(/\\/g, " ").replace(/\"/g, " ").replace(/\</g, " ").replace(/\>/g, " ").replace(/\:/g, " ").replace(/\-/g, " ").replace(/\,/g, " ").replace(/\{/g, " ").replace(/\}/g, " ").replace(/\_/g, " ").replace(/\–/g, " ").replace(/\(/g, " ").replace(/\)/g, " ").replace(/\s+/g, " ");
+
+    // Convert data to text array
+    textArray = text.split(" ");
 
     // Go through each word, and count
-    for(var i = 0; i < text.length; i++) {
+    for(var i = 0; i < textArray.length; i++) {
 
         // Filter 'a', 'the', 'and'
-        if(text[i] != "a" && text[i] != "the" && text[i] != "and") {
+        if(textArray[i] != "a" && textArray[i] != "the" && textArray[i] != "and" && textArray[i] != "") {
 
             // If word exists before, increase count
             for(var j = 0; j < dataAll.length; j++) {
-                if (dataAll[j][0].indexOf(text[i]) > -1) {
+                if (dataAll[j][0].indexOf(textArray[i]) > -1) {
                     dataAll[j][1] += 1;
                     check = 1;
                     break;
@@ -32,7 +45,7 @@ function most_common_words(source, n) {
 
             // Otherwise, just push new text
             if(check != 1) {
-                dataAll.push([text[i], 1]);
+                dataAll.push([textArray[i], 1]);
             }
             check = 0;
         }
@@ -46,12 +59,15 @@ function most_common_words(source, n) {
         else {
             return (a[1] > b[1]) ? -1 : 1;
         }
-    })
+    });
 
     // Show only top n data
-    for (var k = 0; k < n; k++) {
-        console.log("'" + dataAll[k][0] + "': '" + dataAll[k][1] + " occurrences");
+    if(dataAll.length < n) {
+        n = dataAll.length;
+    }
+    for(var k = 0; k < n; k++) {
+        console.log("'" + dataAll[k][0] + "': " + dataAll[k][1] + " occurrences");
     }
 }
 
-most_common_words("the the the error error error error halo halo what are you doing lanang peter", 3)
+most_common_words("source.txt", 10)
